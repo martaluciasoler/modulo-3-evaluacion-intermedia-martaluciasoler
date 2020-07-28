178b35abf7a0a4fbdbd2e11322dc7ff17a223e3e
Marta Lucía Soler <martadalaber@gmail.com>
2020-07-28 09:01:45 +0200

    docs

---
+++ docs/precache-manifest.8cefb102a832529f72a1433031b1115f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4be52d132a5ded7ba72bba031a501df",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b29f2b5abb6c959d3fe2",
+    "url": "./static/css/main.ecc87316.chunk.css"
+  },
+  {
+    "revision": "bf12df88b15339e3b67b",
+    "url": "./static/js/2.5bf8a30b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5bf8a30b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b29f2b5abb6c959d3fe2",
+    "url": "./static/js/main.f41aab36.chunk.js"
+  },
+  {
+    "revision": "a0e6f4cee20c0c79b2e6",
+    "url": "./static/js/runtime-main.8bb1f59a.js"
+  }
+]);
